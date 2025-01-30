5cdb78ad195520bdb10d1ceac9c05973c5972faa
Ubuntu <ubuntu@ip-172-31-68-150.ec2.internal>
2025-01-30 12:40:37 +0000

    2nd push

--- snetsdk-setup.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-git clone git@github.com:singnet/snet-sdk-python.git
-cd snet-sdk-python
-pip install -r requirements.txt
-pip install -e .
-echo "snet sdk setup done"
---
+++ snetsdk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e  # Exit immediately if any command fails
+
+# Ensure pip is updated
+
+pip install --upgrade pip
+
+# Clone the repository if it doesn’t exist
+if [ ! -d "snet-sdk-python" ]; then
+    git clone --branch v3.6.1 https://github.com/singnet/snet-sdk-python.git
+else
+    echo "Repo already exists. Skipping clone."
+fi
+
+# Enter the directory
+cd snet-sdk-python || { echo "Failed to enter directory"; exit 1; }
+
+# Install dependencies
+pip install -r requirements.txt
+pip install -e .
+
+echo "snet sdk setup done"
